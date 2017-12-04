66021567a98061a8b5edf505ca21fd2509f5bb04
DexDexinWang <edgar_wdx@hotmail.com>
2017-12-04 06:23:28 -0800

    Implementing 'extract informaiont from JSON' in TicketMaster API

--- src/external/TicketMasterAPI.java
+++ src/external/TicketMasterAPI.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -22,7 +23,7 @@ public class TicketMasterAPI {
 	private static final String DEFAULT_TERM = "";  // no restriction
 	private static final String API_KEY = "vdIdLhDohOWdwL6y41SKJxQPbudMARpw";
 	
-	public JSONArray search(double lat, double lon, String term) {
+	public List<Item> search(double lat, double lon, String term) {
 		//get first part of url 
 		String url = API_PROTOCOL + API_HOST + SEARCH_PATH;
 		//Add Terms
@@ -59,7 +60,7 @@ public class TicketMasterAPI {
 			JSONObject responseJson = new JSONObject(response.toString());
 			JSONObject embedded = (JSONObject) responseJson.get("_embedded");
 			JSONArray events = (JSONArray) embedded.get("events");
-			return events;
+			return getItemList(events);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -68,24 +69,111 @@ public class TicketMasterAPI {
 	
 	private List<Item> getItemList(JSONArray events) throws JSONException {
 		List<Item> itemList = new ArrayList<>();
-		ItemBuilder builder = new ItemBuilder();
+		for (int i = 0; i <events.length(); i++)
+		{
+			JSONObject event = events.getJSONObject(i);
+			ItemBuilder builder = new ItemBuilder();
+			builder.setItemId(getStringFieldOrNull(event,"id"));
+			builder.setName(getStringFieldOrNull(event,"name"));
+			builder.setDescription(getDescription(event));
+			builder.setCategories(getCategories(event));
+			builder.setImageUrl(getImageUrl(event));
+			builder.setUrl(getStringFieldOrNull(event, "url"));
+			JSONObject venue = getVenue(event);
+			if (venue != null) {
+				if (!venue.isNull("address")) {
+					JSONObject address = venue.getJSONObject("address");
+					StringBuilder sb = new StringBuilder();
+					if (!address.isNull("line1")) {
+						sb.append(address.getString("line1"));
+					}
+					if (!address.isNull("line2")) {
+						sb.append(address.getString("line2"));
+					}
+					if (!address.isNull("line3")) {
+						sb.append(address.getString("line3"));
+					}
+					builder.setAddress(sb.toString());
+				}
+				if (!venue.isNull("city")) {
+					JSONObject city = venue.getJSONObject("city");
+					builder.setCity(getStringFieldOrNull(city, "name"));
+				}
+				if (!venue.isNull("country")) {
+					JSONObject country = venue.getJSONObject("country");
+					builder.setCountry(getStringFieldOrNull(country, "name"));
+				}
+				if (!venue.isNull("state")) {
+					JSONObject state = venue.getJSONObject("state");
+					builder.setState(getStringFieldOrNull(state, "name"));
+				}
+				builder.setZipcode(getStringFieldOrNull(venue, "postalCode"));
+				if (!venue.isNull("location")) {
+					JSONObject location = venue.getJSONObject("location");
+					builder.setLatitude(getNumericFieldOrNull(location, "latitude"));
+					builder.setLongitude(getNumericFieldOrNull(location, "longitude"));
+				}
 
+			}
+			Item item = builder.build();
+			itemList.add(item);
+		}
 		return itemList;
 	}
 	
+	//get address and lot and log
 	private JSONObject getVenue(JSONObject event) throws JSONException {
+		if(!event.isNull("_embedded")) {
+			JSONObject embedded = event.getJSONObject("_embedded");
+			if(!embedded.isNull("venues")) {
+				JSONArray venues = embedded.getJSONArray("venues");
+				if (venues.length() >= 1) {
+					return venues.getJSONObject(0);
+				}
+			}
+		}
 		return null;
 	}
 	
 	private String getImageUrl(JSONObject event) throws JSONException {
+		if (!event.isNull("image")) {
+			JSONArray imagesArray = event.getJSONArray("images");
+			if(imagesArray.length() >= 1) {
+				return getStringFieldOrNull(imagesArray.getJSONObject(0),"url");
+			}
+		}
 		return null;
 	}
 	
-	private String GetDescription(JSONObject event) throws JSONException {
+	private String getDescription(JSONObject event) throws JSONException {
+		if (!event.isNull("description")) {
+			return event.getString("description");
+		} else if (!event.isNull("additionalInfo")) {
+			return event.getString("additionalInfo");
+		} else if (!event.isNull("info")) {
+			return event.getString("info");
+		} else if (!event.isNull("pleaseNote")) {
+			return event.getString("pleaseNote");
+		}
 		return null;
+
 	}
 	
 	private Set<String> getCategories(JSONObject event) throws JSONException {
+		Set<String> categories = new HashSet<String>();
+		if (!event.isNull("classifications")) {
+			JSONArray classifications = (JSONArray) event.get("classifications");
+			for(int i = 0; i < classifications.length(); i++) {
+				if (!classifications.isNull(i)) {
+					JSONObject classification = classifications.getJSONObject(i);
+					if (!classification.isNull("segment")) {
+						JSONObject segment = classification.getJSONObject("segment");
+						categories.add(segment.getString("name"));
+					}
+				}
+			}
+			return categories;
+		}
 		return null;
 	}
 	
@@ -109,11 +197,11 @@ public class TicketMasterAPI {
 	
 	//debug, get JSON from TicketMaster and print
 	private void queryAPI(double lat, double lon) {
-		JSONArray events = search(lat, lon, null);
+		List<Item> itemList = search(lat, lon, null);
 		try {
 			//traverse all value from events
-			for (int i = 0; i < events.length(); i++) {
-				JSONObject obj = events.getJSONObject(i);
+			for (Item item : itemList) {
+				JSONObject obj = item.toJSONObject();
 				System.out.println(obj);
 			}
 		} catch (Exception e) {
